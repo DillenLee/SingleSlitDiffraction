d98414c8eb16091d0c1519f0911e8f912c09a5e2
Dillen Lee <42581500+DillenLee@users.noreply.github.com>
2021-03-25 20:49:06 +0000

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from scipy.optimize import curve_fit
 
+path = '/home/dillen/University/Python/Single Slit diffraction/'
 
 #import the data
-distance, voltage = np.loadtxt('data.csv', delimiter=',',unpack=True)
+distance, voltage = np.loadtxt(path + 'data.csv', delimiter=',',unpack=True)
 distance  = [x-15 for x in distance]
     
     
@@ -42,7 +43,6 @@ def error(x,voltReading):
         wavelengthError = np.abs(((-3*x*np.cos(200*x)+(3/200)*np.sin(200*x))*np.sinc(200*x))/(900*x))     
     #then calculate multimeter error
     voltmeterError = (voltReading*0.05+1)
-    print(voltmeterError)
     return wavelengthError+voltmeterError
 
 
@@ -62,26 +62,44 @@ largesample = np.linspace(-19,19,100)
 for val in largesample:
     datafit.append(intensityFunc(val, *fit))
     
-print(np.sqrt(cov[0][0]))
-print(fit[0])
+print("The fitted aperture for the graphed data is %f μm with an uncertainty of %f μm"%(fit[0]*(10**3),np.sqrt(cov[0][0])*(10**3)))
 
 
 
-
-
-
-#%%
 #-------
 #import the anaylsed data
-distanceAnalysed, greyAnalysed = np.loadtxt('Slit3.csv',delimiter=',',skiprows=1,unpack=True)
-pixelTommRatio = 1/192
-distanceAnalysed = [(x*pixelTommRatio)-10/3 for x in distanceAnalysed]
-greyAnalysed = [(x*0.10) for x in greyAnalysed]
+distanceAnalysed, greyAnalysed = np.loadtxt('Slit2.csv',delimiter=',',skiprows=1,unpack=True)
+pixelTommRatio = 6.656/1280
+distanceAnalysed = [((x-600)*pixelTommRatio) for x in distanceAnalysed]
+greyAnalysed = [(x*1) for x in greyAnalysed]
+
+
+
+#fit a curve to it right 
+f=160
+fitImage,covImage = curve_fit(intensityFunc,distanceAnalysed,greyAnalysed,guess)
+xImage = np.linspace(distanceAnalysed[0],distanceAnalysed[-1],100)
+yImage = intensityFunc(xImage,*fitImage)
+print("The fitted aperture for the analysed image is %f μm with an uncertainty of %f μm"%(fitImage[0]*(10**3),np.sqrt(covImage[0][0])*(10**3)))
+
+
+print(distanceAnalysed[greyAnalysed.index(np.min(greyAnalysed))])
 
 #plot the functions
 
-plt.plot(distanceAnalysed,greyAnalysed)
+#This plots the analysed image graph
+plt.plot(distanceAnalysed,greyAnalysed,'green')
+plt.plot(xImage,yImage,'red')
+plt.legend(['Analysed Data','Fitted Curve'])
+plt.grid()
+plt.title('Plotted graph of analysed image')
+plt.xlabel("Distance from Centre maxima (mm)")
+plt.ylabel("Intensity (arbitrary units)")
+plt.savefig('analysed Image.png')
+plt.show()
 
+
+#this plots the measured data graph
 plt.plot(largesample, datafit)
 plt.errorbar(distance,voltage,yerr=totalErr,fmt='r|',capsize=4)
 plt.grid()
@@ -89,3 +107,5 @@ plt.title('Graph of Voltage measured as a function of distance from maxima')
 plt.xlabel('Distance from the centre of the central maxima (mm)')
 plt.ylabel('Voltage (mV)')
 plt.legend(('Modeled fit', 'Data Points'))
+plt.savefig('graphed data.png')
+plt.show()
